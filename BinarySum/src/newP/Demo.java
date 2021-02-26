894d7c7ac2c9c3a9428d136064906741d78d2cfb
Bashar alkhateeb <79662882+BasharAlkhateeb@users.noreply.github.com>
2021-02-26 22:01:55 +0200

    Add files via upload

---
+++ BinarySum/src/newP/Demo.java
@@ -0,0 +1,15 @@
+
+package newP;
+
+import java.util.Scanner;
+
+
+public class Demo {
+
+     
+    public static void main(String[] args) {
+     Objects a = new Objects();
+     /////example
+        System.out.println(a.binarySum(110, 111));
+}
+}
